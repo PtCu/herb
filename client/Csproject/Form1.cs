264fab8f0074c84b7852d8c955f61dc30deaec5a
PtCu <1365699009@qq.com>
2021-03-30 23:07:36 +0800

    rebuilding

--- client/Csproject/Form1.cs
+++ client/Csproject/Form1.cs
@@ -20,20 +20,19 @@ namespace Csproject
     public partial class Form1 : Form
     {
         private SerialPort port = null;
-        string w_set = "18";
-        string s_set = "40";
-        string g_set = "600";
-        string y_set = "1010";
-        string str = "";
         string w_value = "";
         string s_value = "";
         string g_value = "";
         string y_value = "";
+        string w_set = "100";
+        string s_set = "100";
+        string g_set = "100";
+        string y_set = "100";
         FilterInfoCollection videoDevices;
         VideoCaptureDevice videoSource;
           //创建 1个客户端套接字 和1个负责监听服务端请求的线程  
- 19         Thread threadclient = null;
- 20         Socket socketclient = null;
+         Thread threadclient = null;
+         Socket socketclient = null;
         private struct RootObject
         {
             public string w { get; set; }
@@ -73,7 +72,7 @@ namespace Csproject
                 //客户端套接字连接到网络节点上，用的是Connect
                 socketclient.Connect(point);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 MessageBox.Show("无法连接远程服务器：" + ex.Message, "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
@@ -86,7 +85,7 @@ namespace Csproject
 
         }
         //发送字符信息到服务端的方法  
-       private void ClientSendMsg(string sendMsg)
+       private void SendToServer(string sendMsg)
          {
              //将输入的内容字符串转换为机器可以识别的字节数组     
              byte[] arrClientSendMsg = Encoding.UTF8.GetBytes(sendMsg);
@@ -110,6 +109,10 @@ namespace Csproject
 
                     //将套接字获取到的字符数组转换为人可以看懂的字符串
                     string strRevMsg = Encoding.UTF8.GetString(arrRecvmsg, 0, length);
+
+                    //TODO:
+                    //传送至arduino
+                    UpdateSet(strRevMsg);
                     
                 }
                 catch (Exception ex)
@@ -119,6 +122,7 @@ namespace Csproject
                 }
             }
         }
+       
         //初始化chart
         private void InitChart()
         {
@@ -151,7 +155,7 @@ namespace Csproject
             }
         }
 
-        //初始化form1
+        //初始化摄像
         private void Form1_Load_1(object sender, EventArgs e)
         {
             videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
@@ -160,34 +164,46 @@ namespace Csproject
             videoSourcePlayer1.VideoSource = videoSource;
             videoSourcePlayer1.Start();
         }
+        //关闭摄像
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            videoSourcePlayer1.Stop();
+        }
         //定时器触发回调函数
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (port != null && port.IsOpen)
+      /*      if (port != null && port.IsOpen)
             {
                 port.WriteLine(w_set + '|' + s_set + '|' + g_set + '|' + y_set);
-            }
-            httpToServer(getJson());
+            }*/
+            string data = getData();
+            SendToServer(data);
+           // httpToServer(getJson());
         }
-        //按钮事件回调函数
+        private void UpdateSet(string data)
+        {
+
+        }
+        //提交本地设定（设定温度等）
         private void toolStripButton1_Click(object sender, EventArgs e)
         {
             try
             {
-                double set_w = double.Parse(toolStripTextBox1.Text);
+       /*         double set_w = double.Parse(toolStripTextBox1.Text);
                 set_w = double.Parse(toolStripTextBox2.Text);
                 set_w = double.Parse(toolStripTextBox3.Text);
-                set_w = double.Parse(toolStripTextBox4.Text);
+                set_w = double.Parse(toolStripTextBox4.Text);*/
                 w_set = toolStripTextBox1.Text;
                 s_set = toolStripTextBox2.Text;
                 g_set = toolStripTextBox3.Text;
                 y_set = toolStripTextBox4.Text;
 
-                var objects = new { id = 1, w = w_set, s = s_set, g = g_set, y = (Convert.ToDouble(y_set) / 1000) };
+                UpdateSet(w_set+"|"+s_set+"|"+g_set+"|"+y_set);
+                /*var objects = new { id = 1, w = w_set, s = s_set, g = g_set, y = (Convert.ToDouble(y_set) / 1000) };
                 string strings = JsonConvert.SerializeObject(objects);
                 //string _url = "http://localhost:8080/iotSystem/monitorc/";
-                string _url = "http://localhost:1900/monitorc/";
-                var request = (HttpWebRequest)WebRequest.Create(_url);
+                string _url = "http://localhost:1900/monitorc/";*/
+                /*var request = (HttpWebRequest)WebRequest.Create(_url);
                 request.Method = "POST";
                 request.ContentType = "application/json;charset=UTF-8";
                 byte[] byteData = Encoding.UTF8.GetBytes(strings);
@@ -201,7 +217,7 @@ namespace Csproject
                 RootObject objects2 = JsonConvert.DeserializeObject<RootObject>(responseString.ToString());
 
                 w_set = objects2.w; s_set = objects2.s; g_set = objects2.g; y_set = Convert.ToString((int)(double.Parse(objects2.y) * 1000));
-                textBox1.AppendText("set:" + w_set + ' ' + s_set + ' ' + g_set + ' ' + y_set + "\r\n");
+                textBox1.AppendText("set:" + w_set + ' ' + s_set + ' ' + g_set + ' ' + y_set + "\r\n");*/
 
             }
             catch (Exception ex)
@@ -210,11 +226,13 @@ namespace Csproject
             }
         }
 
+        //关闭摄像
         private void toolStripButton2_Click(object sender, EventArgs e)
         {
             videoSourcePlayer1.Stop();
         }
 
+        //捕捉图形
         private void toolStripButton3_Click(object sender, EventArgs e)
         {
             try
@@ -224,12 +242,39 @@ namespace Csproject
                 if (!Directory.Exists(fileCapturePath))
                     Directory.CreateDirectory(fileCapturePath);*/
                 //抓到图保存到指定路径
-                httpToServer(getJson());
+               /* string data = getData();
+                SendToServer(data);*/
             }
             catch (Exception ex)
             {
                 MessageBox.Show("捕获图像失败！" + ex.Message, "提示");
             }
+        }
+      
+     
+
+        private string getData()
+        {
+            Image img = null;
+            img = videoSourcePlayer1.GetCurrentVideoFrame();
+
+            if (img == null)
+            {
+                img = new Bitmap("test.jpg");
+
+            }
+
+            //img = new Bitmap("test.jpg");
+            MemoryStream ms = new MemoryStream();
+            img.Save(ms, ImageFormat.Jpeg);
+            byte[] bytes = ms.ToArray();
+            ms.Close();
+            //img.Save(fileCapturePath + fileImageName, ImageFormat.Jpeg);
+            string strbaser64 = Convert.ToBase64String(bytes);
+            //string imgStr = "data:image/jpg;base64," + strbaser64;
+            var objects = new { id = 1, w = w_value, s = s_value, g = g_value, y = y_value, img = strbaser64 };
+            string strings = JsonConvert.SerializeObject(objects);
+            return strings;
         }
         void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
@@ -238,7 +283,7 @@ namespace Csproject
             {
                 if (port != null && port.BytesToRead > 0)
                 {
-                    str = port.ReadLine();
+                    string str = port.ReadLine();
                     string[] sArray = str.Split('|');
                     w_value = sArray[0];
                     s_value = sArray[1];
@@ -277,69 +322,39 @@ namespace Csproject
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message, "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show(ex.Message, "读取串口错误，提示信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            videoSourcePlayer1.Stop();
-        }
+        /*        private void httpToServer(string strings)
+                {
+                    //string _url = "http://localhost:8080/iotSystem/monitor/";
+                    string _url = "http://localhost:1900/monitor/";
+                    var request = (HttpWebRequest)WebRequest.Create(_url);
+                    request.Method = "POST";
+                    request.ContentType = "application/json;charset=UTF-8";
+                    byte[] byteData = Encoding.UTF8.GetBytes(strings);
+                    int length = byteData.Length;
+                    request.ContentLength = length;
+                    Stream writer = request.GetRequestStream();
+                    writer.Write(byteData, 0, length);
+                    writer.Close();
+
+                    var response = (HttpWebResponse)request.GetResponse();
+                    var responseString = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8")).ReadToEnd();
+                    RootObject objects2 = JsonConvert.DeserializeObject<RootObject>(responseString.ToString());
+                    if (w_set != objects2.w || s_set != objects2.s || g_set != objects2.g ||
+                        y_set != Convert.ToString((int)(double.Parse(objects2.y) * 1000)))
+                    {
+                        w_set = objects2.w; s_set = objects2.s; g_set = objects2.g;
+                        y_set = Convert.ToString((int)(double.Parse(objects2.y) * 1000));
+                        textBox1.AppendText("set:" + w_set + ' ' + s_set + ' ' + g_set + ' ' + y_set + "\r\n");
+                    }
 
 
-        private string getJson()
-        {
-            Image img = null;
-            img = videoSourcePlayer1.GetCurrentVideoFrame();
-
-            if (img == null)
-            {
-                img = new Bitmap("test.jpg");
-
-            }
-
-            //img = new Bitmap("test.jpg");
-            MemoryStream ms = new MemoryStream();
-            img.Save(ms, ImageFormat.Jpeg);
-            byte[] bytes = ms.ToArray();
-            ms.Close();
-            //img.Save(fileCapturePath + fileImageName, ImageFormat.Jpeg);
-            string strbaser64 = Convert.ToBase64String(bytes);
-            //string imgStr = "data:image/jpg;base64," + strbaser64;
-            var objects = new { id = 1, w = w_value, s = s_value, g = g_value, y = y_value, img = strbaser64 };
-            string strings = JsonConvert.SerializeObject(objects);
-            return strings;
-        }
-
-        private void httpToServer(string strings)
-        {
-            //string _url = "http://localhost:8080/iotSystem/monitor/";
-            string _url = "http://localhost:1900/monitor/";
-            var request = (HttpWebRequest)WebRequest.Create(_url);
-            request.Method = "POST";
-            request.ContentType = "application/json;charset=UTF-8";
-            byte[] byteData = Encoding.UTF8.GetBytes(strings);
-            int length = byteData.Length;
-            request.ContentLength = length;
-            Stream writer = request.GetRequestStream();
-            writer.Write(byteData, 0, length);
-            writer.Close();
-        
-            var response = (HttpWebResponse)request.GetResponse();
-            var responseString = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8")).ReadToEnd();
-            RootObject objects2 = JsonConvert.DeserializeObject<RootObject>(responseString.ToString());
-            if (w_set != objects2.w || s_set != objects2.s || g_set != objects2.g ||
-                y_set != Convert.ToString((int)(double.Parse(objects2.y) * 1000)))
-            {
-                w_set = objects2.w; s_set = objects2.s; g_set = objects2.g;
-                y_set = Convert.ToString((int)(double.Parse(objects2.y) * 1000));
-                textBox1.AppendText("set:" + w_set + ' ' + s_set + ' ' + g_set + ' ' + y_set + "\r\n");
-            }
+                }*/
 
 
-        }
 
-
-   
 
     }
 }
