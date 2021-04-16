bb5a9001e7fb17cf18d07ee457659c8b8e519b75
PtCu <1365699009@qq.com>
2021-04-16 20:32:00 +0800

    finish arduino

--- arduino/CommunicateWithPC/CommunicateWithPC.ino
+++ arduino/CommunicateWithPC/CommunicateWithPC.ino
@@ -3,22 +3,31 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x20, 16, 2);
 dht11 DHT;
-#define DHT11_PIN 4
+#define LIGHT_PIN 0
+#define FLAME_PIN 1
+#define DHT11_PIN 2
+
+#define HEAT_PIN 1
+#define HUMI_PIN 3
+#define LIGHT_PIN 5
+#define PRESS_PIN 7
+
 boolean readCompleted = false;
 boolean open[4];
-
+int ledPin = 3; // LED connected to digital pin 3
 void setup()
 {
   Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop()
 {
   int chk = DHT.read(DHT11_PIN);
   int ligt_intensity;
-  ligt_intensity = analogRead(0);
+  ligt_intensity = analogRead(LIGHT_PIN);
   //从传感器读取数据到串口
-  int flameSensor = analogRead(1);
+  int flameSensor = analogRead(FLAME_PIN);
   Serial.print(DHT.temperature, 1);
   Serial.print('|');
   Serial.print(DHT.humidity, 1);
@@ -34,26 +43,46 @@ void loop()
     if (open[0])
     {
       //TODO:开启加热装置
+      digitalWrite(HEAT_PIN, HIGH);
       Serial.print("heat\n");
+    }
+    else
+    {
+      digitalWrite(HEAT_PIN, LOW);
     }
     if (open[1])
     {
-      //TODO:开启加热装置
+      //TODO:开启加湿装置
+      digitalWrite(HUMI_PIN, HIGH);
       Serial.print("humi\n");
+    }
+    else
+    {
+      digitalWrite(HUMI_PIN, LOW);
     }
     if (open[2])
     {
-      //TODO:开启加热装置
+      //TODO:开启加光装置
+      digitalWrite(LIGHT_PIN, HIGH);
       Serial.print("light\n");
+    }
+    else
+    {
+      digitalWrite(LIGHT_PIN, LOW);
     }
     if (open[3])
     {
-      //TODO:开启加热装置
+      //TODO:开启加压装置
+      digitalWrite(PRESS_PIN, HIGH);
       Serial.print("press\n");
+    }
+    else
+    {
+      digitalWrite(PRESS_PIN, LOW);
     }
     readCompleted = false;
   }
-  delay(500);
+  delay(1000);
 }
 void serialEvent()
 {
